b61b1996f46be9b627a42e2427d048ec3d644327
osun <ocean.sun@sensirion.com>
2023-02-05 16:57:59 +0800

    new

--- osunpull.py
+++ osunpull.py
@@ -1,16 +1,3 @@
-print("This is git test!")
-<<<<<<< HEAD
-print("shamerge")
-=======
-print("Dev test")
-print("sha dev")
->>>>>>> 86f0091f1c7882bbef8b7f0bf3f61445aac2e88e
-
-"add one line here"
-
-"one more"
-
-"more"
-"more"
+xxx
 ----
 # ---- note20220123 ----
